75dd902c682d7ac19f1f1d7f52bee577dbd5ed6d
javadsabbagh <j.sabbagh89@gmail.com>
2016-06-14 01:30:06 +0430

    Add files

---
+++ src/main/java/com/dev/web/extr/ui/BrowserController.java
@@ -0,0 +1,32 @@
+package com.dev.web.extr.ui;
+
+import java.awt.Button;
+import java.awt.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+/**
+ *
+ * @author Javad Sabbagh
+ */
+public class BrowserController {
+
+    @FXML
+    WebView browser;
+    @FXML
+    TextField urlField;
+    @FXML
+    Button goButton;
+    @FXML
+    Button aboutButton;
+    
+    @FXML
+    public void onGoAction() {
+        
+    }
+    
+    @FXML
+    public void onAboutAction() {
+        
+    }
+}
